332620e2dd9711463e640e9ea7928e1f3d8fbaea
Mattia Bocchino <pmattiab@live.it>
2023-09-27 20:44:19 +0200

    edit channels

--- js/channels.js
+++ js/channels.js
@@ -12,28 +12,10 @@ var channels = [
         "name": "LIVE 1",
         "class": "channel-pro",
         "id": "",
-        "url": "https://dcb-deit-livedazn.daznedge.net/135beqrv641ty1vr80f9hz141n/web/stream.mpd?channel=1381&outlet=dazn-italy&plang=it",
+        "url": "https://dca-ac-live.cdn.indazn.com/2mxidohj8mp91ci3wf7h6k46x/web/stream.mpd?channel=1382&outlet=dazn-italy&plang=en",
         "keyId": "cfb5e2b73bef4f3c878f25ab86a7451f",
         "key": "0d6712bf2a84edcc93d001a9613f6fec",
         "type": "dash"
-    },
-    {
-        "name": "LIVE 2",
-        "class": "channel-pro",
-        "id": "",
-        "url": "https://dca-ac-live.cdn.indazn.com/7449iprd58tb1jcqh04557ntn/web/stream.mpd?channel=1384&mta=it&outlet=dazn-italy&plang=it",
-        "keyId": "cfb5e2b73bef4f3c878f25ab86a7451f",
-        "key": "0d6712bf2a84edcc93d001a9613f6fec",
-        "type": "dash"
-    },
-    {
-        "name": "24",
-        "class": "channel-simple",
-        "id": "",
-        "url": "https://dastxui.ddns.net:443/play/FL4c08n6PSe2rRIHSYvUJ1mYAc24tccZQxYBHX7uE7I/m3u8",
-        "keyId": "",
-        "key": "",
-        "type": ""
     },
     {
         "name": "24",
@@ -46,30 +28,12 @@ var channels = [
     },
     {
         "name": "UNO",
-        "class": "channel-simple",
-        "id": "",
-        "url": "https://dastxui.ddns.net:443/play/FL4c08n6PSe2rRIHSYvUJ03ia2l3HCfNhQR8Oy_fLMQ/m3u8",
-        "keyId": "",
-        "key": "",
-        "type": ""
-    },
-    {
-        "name": "UNO",
         "class": "channel-pro",
         "id": "",
         "url": "https://linear301-it-dash1-prd.selector.skycdn.it/016a/31023/FHD/skysportuno/master.mpd",
         "keyId": "00362452ed102000ccea64c311921dc2",
         "key": "02f27bd6caab7633d3083b94d3958b1c",
         "type": "dash"
-    },
-    {
-        "name": "F1",
-        "class": "channel-simple",
-        "id": "",
-        "url": "https://dastxui.ddns.net:443/play/FL4c08n6PSe2rRIHSYvUJ-nt765wkIidexlNu042TPs/m3u8",
-        "keyId": "",
-        "key": "",
-        "type": ""
     },
     {
         "name": "STOP",
@@ -79,6 +43,15 @@ var channels = [
         "keyId": "",
         "key": "",
         "type": ""
+    },
+    {
+        "name": "F1",
+        "class": "channel-pro",
+        "id": "",
+        "url": "https://linear307-it-dash1-prd.selector.skycdn.it/016a/31478/FHD/skysportf1/master_stereo.mpd",
+        "keyId": "0036aca3ce896fe790ef07e89b74cae4",
+        "key": "58145ec5b35e7f62555c2ed5f3ef2342",
+        "type": "dash"
     },
     {
         "name": "MOTO",
@@ -91,47 +64,56 @@ var channels = [
     },
     {
         "name": "TENNIS",
-        "class": "channel-simple",
+        "class": "channel-pro",
         "id": "",
-        "url": "",
-        "keyId": "",
-        "key": "",
+        "url": "https://linear310-it-dash1-prd.selector.skycdn.it/016a/32559/FHD/skysporttennis/master.mpd",
+        "keyId": "00360f4f90045f029a53a022738c2422",
+        "key": "fa39fd500dc4e291801a710499634330",
         "type": "dash"
     },
     {
         "name": "ARENA",
-        "class": "channel-simple",
+        "class": "channel-pro",
         "id": "",
-        "url": "",
-        "keyId": "",
-        "key": "",
+        "url": "https://linear304-it-dash1-prd.selector.skycdn.it/016a/31024/FHD/skysportarena/master.mpd",
+        "keyId": "00364ee1c777f44a74e4df12f3095503",
+        "key": "77dee68a39366663755beb8fa62dcc1e",
         "type": "dash"
     },
     {
         "name": "MAX",
-        "class": "channel-simple",
+        "class": "channel-pro",
         "id": "",
-        "url": "",
-        "keyId": "",
-        "key": "",
+        "url": "https://linear305-it-dash1-prd.selector.skycdn.it/016a/31248/FHD/skysportmax/master.mpd",
+        "keyId": "0036a901e6f0381cfd1b6fcf10cc6038",
+        "key": "4fbb022704c9abcbb709484a5667fd79",
         "type": "dash"
     },
     {
         "name": "NBA",
-        "class": "channel-simple",
+        "class": "channel-pro",
         "id": "",
-        "url": "",
-        "keyId": "",
-        "key": "",
+        "url": "https://linear308-it-dash1-prd.selector.skycdn.it/016a/31764/FHD/skysportnba/master.mpd",
+        "keyId": "0036c4e453ae2a044f4c861ed73b560a",
+        "key": "ea5626c0aa092c382ca58151b5f4b208",
         "type": "dash"
     },
     {
         "name": "GOLF",
-        "class": "channel-simple",
+        "class": "channel-pro",
         "id": "",
-        "url": "",
-        "keyId": "",
-        "key": "",
+        "url": "https://linear309-it-dash1-prd.selector.skycdn.it/016a/32768/FHD/skysportgolf/master.mpd",
+        "keyId": "0036a02f08506f8428164d78e3ebb6de",
+        "key": "8708612971e3cfa8e03698996b1a3b7f",
+        "type": "dash"
+    },
+    {
+        "name": "EURO 1",
+        "class": "channel-pro",
+        "id": "",
+        "url": "https://dce-ac-live.cdn.indazn.com/dashdrm/dazn-linear-029/stream.mpd?p=web",
+        "keyId": "82f8e2a17dac44c0a18f660479349c59",
+        "key": "1b144f73e6fefe91cd05f850e2b589d0",
         "type": "dash"
     },
     {
@@ -153,13 +135,13 @@ var channels = [
         "type": "dash"
     },
     {
-        "name": "CALCIO",
-        "class": "channel-simple",
+        "name": "EURO 2",
+        "class": "channel-pro",
         "id": "",
-        "url": "https://dastxui.ddns.net:443/play/FL4c08n6PSe2rRIHSYvUJ33U9PNh2XNmF9hFKaGMlLk/m3u8",
-        "keyId": "",
-        "key": "",
-        "type": ""
+        "url": "https://timlivetu0.cb.ticdn.it/Content/DASH/Live/channel(eurosport2)/manifest.mpd",
+        "keyId": "2abd9ff2cd494f829a607250746a1a08",
+        "key": "879cc8e7a94e046b4ea77537ac2fe0ec",
+        "type": "dash"
     },
     {
         "name": "CALCIO",
@@ -168,120 +150,120 @@ var channels = [
         "url": "https://linear302-it-dash1-prd.selector.skycdn.it/016a/31209/FHD/skysportseriea/master.mpd",
         "keyId": "0036e5d0e88dfb087f1791da6150ced9",
         "key": "0bc653d830837dceb13bd17169f0f8e2",
-        "type": "dash"
+        "type": ""
     },
     {
         "name": "CALCIO 1",
-        "class": "channel-simple",
+        "class": "channel-pro",
         "id": "",
-        "url": "https://dastxui.ddns.net:443/play//m3u8",
-        "keyId": "",
-        "key": "",
-        "type": "dash"
+        "url": "https://linear311-it-dash1-prd.selector.skycdn.it/016a/31917/FHD/skysport251/master.mpd",
+        "keyId": "00368000eaecbeeafd84c05b3b8243af",
+        "key": "8f84185910e18ae0ca8a45004625a000",
+        "type": ""
     },
     {
         "name": "CALCIO 2",
-        "class": "channel-simple",
+        "class": "channel-pro",
         "id": "",
-        "url": "https://dastxui.ddns.net:443/play//m3u8",
-        "keyId": "",
-        "key": "",
+        "url": "https://linear312-it-dash1-prd.selector.skycdn.it/016a/32951/FHD/skysport252/master.mpd",
+        "keyId": "0036c4797608fc64296d437bc6f8b790",
+        "key": "1689af84b23f514005f9bb854da7667f",
         "type": "dash"
     },
     {
         "name": "CALCIO 3",
-        "class": "channel-simple",
+        "class": "channel-pro",
         "id": "",
-        "url": "https://dastxui.ddns.net:443/play//m3u8",
-        "keyId": "",
-        "key": "",
+        "url": "https://linear311-it-dash1-prd.selector.skycdn.it/016a/32233/FHD/skysport253/master.mpd",
+        "keyId": "00365b169a077daac5081277c3fd0c28",
+        "key": "f68ab32a552e13509a8ee1c4d96204bc",
         "type": "dash"
     },
     {
         "name": "CALCIO 4",
-        "class": "channel-simple",
+        "class": "channel-pro",
         "id": "",
-        "url": "https://dastxui.ddns.net:443/play//m3u8",
-        "keyId": "",
-        "key": "",
-        "type": "dash"
+        "url": "https://linear312-it-dash1-prd.selector.skycdn.it/016a/31234/FHD/skysport254/master.mpd",
+        "keyId": "00369c14c20b78aadb1ec0e3c0e74979",
+        "key": "e768767e2c7238d8069887bb36aed7fa",
+        "type": ""
     },
     {
         "name": "CALCIO 5",
-        "class": "channel-simple",
+        "class": "channel-pro",
         "id": "",
-        "url": "https://dastxui.ddns.net:443/play//m3u8",
-        "keyId": "",
-        "key": "",
-        "type": "dash"
+        "url": "https://linear311-it-dash1-prd.selector.skycdn.it/016a/32910/FHD/skysport255/master.mpd",
+        "keyId": "0036b781a22ebb0c20c16ac27d5d1448",
+        "key": "f309b94acfda720bf1ed5741489f8967",
+        "type": ""
     },
-    {
-        "name": "CALCIO 6",
-        "class": "channel-simple",
-        "id": "",
-        "url": "https://dastxui.ddns.net:443/play//m3u8",
-        "keyId": "",
-        "key": "",
-        "type": "dash"
-    },
-    {
-        "name": "CALCIO 7",
-        "class": "channel-simple",
-        "id": "",
-        "url": "https://dastxui.ddns.net:443/play//m3u8",
-        "keyId": "",
-        "key": "",
-        "type": "dash"
-    },
-    {
-        "name": "CALCIO 8",
-        "class": "channel-simple",
-        "id": "",
-        "url": "https://dastxui.ddns.net:443/play//m3u8",
-        "keyId": "",
-        "key": "",
-        "type": "dash"
-    },
-    {
-        "name": "CALCIO 9",
-        "class": "channel-simple",
-        "id": "",
-        "url": "https://dastxui.ddns.net:443/play//m3u8",
-        "keyId": "",
-        "key": "",
-        "type": "dash"
-    },
-    {
-        "name": "CALCIO 10",
-        "class": "channel-simple",
-        "id": "",
-        "url": "https://dastxui.ddns.net:443/play//m3u8",
-        "keyId": "",
-        "key": "",
-        "type": "dash"
-    },
-    {
-        "name": "CALCIO 11",
-        "class": "channel-simple",
-        "id": "",
-        "url": "https://dastxui.ddns.net:443/play//m3u8",
-        "keyId": "",
-        "key": "",
-        "type": "dash"
-    },
-    {
-        "name": "PRIME",
-        "class": "channel-simple",
-        "id": "",
-        "url": "https://dastxui.ddns.net:443/play/FL4c08n6PSe2rRIHSYvUJxvOxquHvkxIhWbqeBwNm3o/m3u8",
-        "keyId": "",
-        "key": "",
-        "type": "dash"
-    }
+    // {
+    //     "name": "CALCIO 6",
+    //     "class": "channel-pro",
+    //     "id": "",
+    //     "url": "",
+    //     "keyId": "",
+    //     "key": "",
+    //     "type": ""
+    // },
+    // {
+    //     "name": "CALCIO 7",
+    //     "class": "channel-pro",
+    //     "id": "",
+    //     "url": "",
+    //     "keyId": "",
+    //     "key": "",
+    //     "type": ""
+    // },
+    // {
+    //     "name": "CALCIO 8",
+    //     "class": "channel-pro",
+    //     "id": "",
+    //     "url": "",
+    //     "keyId": "",
+    //     "key": "",
+    //     "type": ""
+    // },
+    // {
+    //     "name": "CALCIO 9",
+    //     "class": "channel-pro",
+    //     "id": "",
+    //     "url": "",
+    //     "keyId": "",
+    //     "key": "",
+    //     "type": ""
+    // },
+    // {
+    //     "name": "CALCIO 10",
+    //     "class": "channel-pro",
+    //     "id": "",
+    //     "url": "",
+    //     "keyId": "",
+    //     "key": "",
+    //     "type": ""
+    // },
+    // {
+    //     "name": "CALCIO 11",
+    //     "class": "channel-pro",
+    //     "id": "",
+    //     "url": "",
+    //     "keyId": "",
+    //     "key": "",
+    //     "type": ""
+    // },
+    // {
+    //     "name": "PRIME",
+    //     "class": "channel-pro",
+    //     "id": "",
+    //     "url": "https://dastxui.ddns.net:443/play/FL4c08n6PSe2rRIHSYvUJ0PKbI04Hax5h8tPbyvqQsg/m3u8",
+    //     "keyId": "",
+    //     "key": "",
+    //     "type": "dash"
+    // }
 ];
 
 (function() {
-    let container = document.getElementById("channels-container");
+    var container = document.getElementById("channels-container");
     channels.forEach(function(channel, index) {
         container.innerHTML += `<div id="${channel.id}" class="${channel.class}" data-url="${channel.url}" data-keyid="${channel.keyId}" data-key="${channel.key}">${channel.name}</div>`;
     });
