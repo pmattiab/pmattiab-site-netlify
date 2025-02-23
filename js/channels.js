8d97cc018a075f6ee6fd2c64f42187c3f7569fc7
Mattia Bocchino <pmattiab@live.it>
2025-02-23 18:50:16 +0100

    update channels

--- js/channels.js
+++ js/channels.js
@@ -48,10 +48,10 @@ let channels =
 		title: "DAZN", type: "mpd", keyId: "8ab47741930c476780515f9a00decb0a", key: "7ab4b9ae5a48aa526e511a913b832769",
 		file: "https://dcf-de-livedazn.daznedge.net/dash/dazn-linear-024/stream.mpd?p=web", content: "sport"
 	},
-	{
-		title: "DAZN1", type: "mpd", keyId: "82f8e2a17dac44c0a18f660479349c59", key: "1b144f73e6fefe91cd05f850e2b589d0",
-		file: "https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-024/stream.mpd", content: "sport"
-	},
+	// {
+	// 	title: "DAZN1", type: "mpd", keyId: "82f8e2a17dac44c0a18f660479349c59", key: "1b144f73e6fefe91cd05f850e2b589d0",
+	// 	file: "https://dce-ak-livewwdazn.akamaized.net/dashdrm/dazn-linear-024/stream.mpd", content: "sport"
+	// },
 	{
 		title: "SPORT CALCIO", type: "mpd", keyId: "00362f95db61eba0e6f14acee3f71e01", key: "fb74cd84b53c7557e18424a3356c4665",
 		file: "https://linear302-it-dash1-prd.selector.skycdn.it/016a/31209/FHD/skysportseriea/master.mpd", content: "sport"
